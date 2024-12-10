36401370b8e4fa4b71c1024dd728e06c3116b0e8
KimJongJo <desk1614@gmail.com>
2024-12-10 16:10:19 +0900

    first commit

---
+++ JJ_TodoList/src/main/java/com/example/JJ_TodoList/JjTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.JJ_TodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjTodoListApplication.class, args);
+	}
+
+}
---
+++ JJ_TodoList/src/test/java/com/example/JJ_TodoList/JjTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JJ_TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
